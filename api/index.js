4f945db922df3113b6a617adab9808b3ff54b5a6
Ayon Sarkar <ayonsarkar380@gmail.com>
2025-02-10 11:29:05 +0530

    v0.11

--- api/index.js
+++ api/index.js
@@ -1,21 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// Example of an API endpoint
-app.get('/api/greet', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = app;
