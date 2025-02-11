defc78787ec34d12703005cc6dbedaf20faeb920
Ayon Sarkar <ayonsarkar380@gmail.com>
2025-02-11 22:40:08 +0530

    v0.141

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+const pg = require('pg');
 // Fallback to local database if DATABASE_URL is not set
 const dbUrl = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/aptinova_db';
 
 const sequelize = new Sequelize(dbUrl, {
   dialect: 'postgres',
+  dialectModule: pg,
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   ssl: process.env.NODE_ENV === 'production',
   dialectOptions: process.env.NODE_ENV === 'production' ? {
